8521f6dbb310d8aa9322bdd52b2674e2cd987638
haretaro <frontend.1000@gmail.com>
2016-01-05 22:42:36 +0900

    lcd_number 使ってみた

--- timer.py
+++ timer.py
@@ -5,13 +5,19 @@ class CountDownWidget(QWidget):
     def __init__(self, parent=None):
         QWidget.__init__(self, parent=parent)
         self.interval = 10
+        self.count = 0
         self.timer = QTimer(parent=self)
         self.timer.setInterval(self.interval)
-        self.timer.timeout.connect(self.do_countdown)
+        self.timer.timeout.connect(self.do_count)
+        self.lcd_number = QLCDNumber(parent=self)
+        layout = QVBoxLayout()
+        layout.addWidget(self.lcd_number)
+        self.setLayout(layout)
         self.timer.start()
 
-    def do_countdown(self):
-        print('hoge')
+    def do_count(self):
+        self.count += 1
+        self.lcd_number.display(self.count)
 
 if __name__ == '__main__':
     import sys
